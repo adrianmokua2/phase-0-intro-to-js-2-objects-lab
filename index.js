3ba639c854de49492540caff071c233a723b639d
Adrian <adrian.mokua@icloud.com>
2023-09-15 11:43:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+public class Employee {
+    private String name;
+    private String streetAddress;
+
+    public Employee(String name, String streetAddress) {
+        this.name = name;
+        this.streetAddress = streetAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", streetAddress=" + streetAddress + "]";
+    }
+}
